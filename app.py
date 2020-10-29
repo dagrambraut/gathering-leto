97182e133a2fa26675d2910ae8ee445af87976cb
Dag Rambraut <DAGRA@equinor.com>
2020-10-29 14:35:45 +0100

    API to get issues count from leto and index.html

--- app.py
+++ app.py
@@ -2,43 +2,22 @@ import os
 from flask import Flask, render_template
 import requests
 
+import github
+
 app = Flask("name-gathering")
+g = github.Github()
+
+@app.route("/get_leto_issue_count/")
+def get_leto_issue_count():
+
+    repo = g.get_repo("equinor/gathering-leto")
+    issues = repo.get_issues()
+
+    return str(issues.totalCount)
 
 @app.route("/")
 def root():
-
-    return render_template("index.html")
-
-
-@app.route("/getleto/")
-def getleto():
-    url = f"https://api.github.com/repos/equinor/gathering-leto"
-    repo = requests.get(url).json()
-    return repo
-
-@app.route("/getissues/")
-def getissues():
-    url = f"https://api.github.com/repos/equinor/gathering-leto/issues/0"
-    res = requests.get(url).json()
-    return res
-
-@app.route("/getrepos/")
-def getrepos():
-    url = f"https://api.github.com/users/markusdregi/repos"
-    user_repos = requests.get(url).json()
-    return user_repos[0]
-
-@app.route("/getuser/")
-def getuser():
-    # github username
-    username = "markusdregi"
-    # url to request
-    url = f"https://api.github.com/users/{username}"
-    # make the request and return the json
-    user_data = requests.get(url).json()
-    # pretty print JSON data
-    return user_data
-
+    return render_template("index.html", issue_count = get_leto_issue_count())
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8000)
